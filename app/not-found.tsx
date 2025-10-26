5c801a6c25dfc25df14857838dfaa5adb10f9aeb
Brecht Cloetens <brecht@codeurs.be>
2025-10-26 08:53:38 +0100

    Added custom 404

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="inline-block text-2xl font-medium align-top pr-12 mr-12 leading-[49px] border-r border-gray-300 dark:border-gray-600">
+        404
+      </h1>
+      <div className="inline-block">
+        <h2 className="text-sm font-normal leading-[49px] m-0">
+          This page could not be found.
+        </h2>
+      </div>
+    </div>
+  );
+}
