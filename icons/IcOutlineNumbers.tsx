62b2bd4890064290c06d6a6e0bf1da5b642567d2
Brecht Cloetens <brecht@codeurs.be>
2025-11-02 21:57:49 +0100

    Finished set cards

---
+++ icons/IcOutlineNumbers.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export function IcOutlineNumbers(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      {/* Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE */}
+      <path
+        fill="currentColor"
+        d="m20.5 10l.5-2h-4l1-4h-2l-1 4h-4l1-4h-2L9 8H5l-.5 2h4l-1 4h-4L3 16h4l-1 4h2l1-4h4l-1 4h2l1-4h4l.5-2h-4l1-4zm-7 4h-4l1-4h4z"
+      />
+    </svg>
+  );
+}
+export default IcOutlineNumbers;
