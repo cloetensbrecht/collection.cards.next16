f6dd230e53c970feb6abe416d61f36518c2970be
Brecht Cloetens <brecht@codeurs.be>
2025-11-11 08:28:01 +0100

    Added de cardgrid component to the card detail page

--- app/collections/[collection]/[series]/[serie]/[set]/[card]/(card)/page.tsx
+++ app/collections/[collection]/[series]/[serie]/[set]/[card]/(card)/page.tsx
@@ -1,17 +1,64 @@
 import { PokemonCard } from "@/alinea/schemas/PokemonCard";
 import { cms } from "@/cms";
+import CardGrid, { CardGridProps } from "@/components/cardgrid/CardGrid";
 import Container from "@/components/container/Container";
 import { Title } from "@/components/title/Title";
+import { blurDataURL } from "@/lib/blurDataURL";
 import { notFound } from "next/navigation";
 
 const fetchCardData = async (url: string) => {
-  return await cms.first({
+  const data = await cms.first({
     type: PokemonCard,
     filter: {
       _status: "published",
       _url: url,
     },
   });
+
+  if (!data) return null;
+
+  const cards = [] as CardGridProps["cards"];
+
+  const basicInfo: CardGridProps["cards"][number] = {
+    blurDataURL: blurDataURL(data.card?.thumbHash),
+    edgeColor: data.edgeColor,
+    focus: data.card?.focus,
+    glowColor:
+      data.energy || data.subtype
+        ? `var(--${data.energy || data.subtype})`
+        : undefined,
+    id: data._id,
+    src: `/media${data.card.src}`,
+    title: data.title,
+    variant: "normal",
+  };
+
+  // there are no variants, add the normal card
+  if (!data.variants || data.variants.length === 0) {
+    cards.push(basicInfo);
+  }
+
+  // add the variants
+  data.variants?.forEach((variant) => {
+    cards.push({
+      ...basicInfo,
+      id: variant._id,
+      foil: variant.foil?.src || undefined,
+      mask: variant.mask?.src || undefined,
+      pattern: variant.pattern || undefined,
+      src:
+        variant.variant === "reverse_holofoil" && data.reverseCard?.src
+          ? `/media${data.reverseCard?.src}`
+          : basicInfo.src,
+      title: `${basicInfo.title}`,
+      variant: variant.variant || "normal",
+    });
+  });
+
+  return {
+    ...data,
+    cards,
+  };
 };
 
 export default async function Card({
@@ -36,6 +83,7 @@ export default async function Card({
       <Title.H1>
         {cardData.title} ({cardData.number})
       </Title.H1>
+      <CardGrid cards={cardData.cards} />
     </Container>
   );
 }
